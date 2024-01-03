11a36e052e2f0ad38e6a42d38000d6906383833f
Piyum Kaveeshwara <piyum7020@gmail.com>
2024-01-03 22:07:59 +0530

    created bookModel and memberModel

---
+++ backend/models/bookModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+  {
+    bookName: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    isbn: {
+      type: Number,
+      required: true,
+    },
+    publishedDate: {
+      type: Date,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("book", bookSchema);
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,10 +1,41 @@
 const express = require("express");
+const Book = require("../models/bookModel");
 
 const router = express.Router();
 
 // GET all books
 router.get("/", (req, res) => {
   res.json({ msg: "GET all books" });
+});
+
+// POST book
+router.post("/", async (req, res) => {
+  const {
+    bookName,
+    author,
+    publisher,
+    isbn,
+    publishedDate,
+    language,
+    image,
+    description,
+  } = req.body;
+
+  try {
+    const book = await Book.create({
+      bookName,
+      author,
+      publisher,
+      isbn,
+      publishedDate,
+      language,
+      image,
+      description,
+    });
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //GET single books
