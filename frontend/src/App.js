b96b65250f96a40565517ec3b10e0f7bf2ddc8e4
Piyum Kaveeshwara <piyum7020@gmail.com>
2024-01-04 20:33:22 +0530

    begin frontend development

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// pages and components
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
