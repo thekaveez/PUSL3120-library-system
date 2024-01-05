56f2b0e7f7abb7f2d20f8cca518ec316cc053b5d
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-01-06 00:31:18 +0530

    add about us and contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import NavbarComponent from "./components/Navbar";
 import Signup from "./components/Signup";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Signup />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </BrowserRouter>
