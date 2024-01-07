40583133c0a06b01e3351f5e6c01245794d9a0f7
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-01-07 16:27:04 +0530

    pop up done

--- Admin/src/pages/IssuedBooks.js
+++ Admin/src/pages/IssuedBooks.js
@@ -1,12 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Modal from "react-modal";
 
 const IssuedBooks = () => {
-    return (
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleEdit = (book) => {
+    setSelectedBook(book);
+    setModalIsOpen(true);
+    console.log(`Edit book with ID ${book.id}`);
+  };
+
+  const handleDelete = (bookId) => {
+    console.log(`Delete book with ID ${bookId}`);
+  };
+
+  const handleReturn = (book) => {
+    console.log(`Return book with ID ${book.id}`);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("/api/books");
+        if (!response.ok) {
+          throw Error("Failed to fetch books");
+        }
+        const json = await response.json();
+        setBooks(json);
+      } catch (error) {
+        console.error("Error fetching books", error.message);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  return (
+    <div className={"main_books container_flex_col"}>
+      <div className={"books_header"}></div>
+      <div className={"books_table_section"}>
+        <table>
+          <thead>
+            <tr>
+              <th className={"col_1"}>Book Name</th>
+              <th className={"col_2"}>Member ID</th>
+              <th className={"col_3"}>Issued Date</th>
+              <th className={"col_4"}>Due Date</th>
+              <th className={"col_5"}></th>
+              <th className={"col_6"}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {books &&
+              books.map((book) => (
+                <tr key={book.id}>
+                  <td>{book.bookName}</td>
+                  <td>{book.author}</td>
+                  <td>{book.isbn}</td>
+                  <td>{book.publishedDate}</td>
+                  <td>
+                    <button className={"btn_books"} onClick={() => handleEdit(book)}>
+                      <FontAwesomeIcon icon={faEdit} />
+                    </button>
+                  </td>
+                  <td>
+                    {/* Return button */}
+                    <button className={"btn_books btn_return"} onClick={() => handleReturn(book)}>
+                      Return
+                    </button>
+                  </td>
+                  {/* <td>
+                    <button className={"btn_books"} onClick={() => handleDelete(book.id)}>
+                      <FontAwesomeIcon icon={faTrashAlt} />
+                    </button>
+                  </td> */}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} ariaHideApp={false}>
         <div>
-            <h2>Page 3</h2>
-            <p>Content for Page 3</p>
+          <h2>Edit Book</h2>
+          {selectedBook && (
+            <div>
+              <p> Book Name: {selectedBook.bookName}</p>
+              <p>Author: {selectedBook.author}</p>
+              <p>ISBN Number: {selectedBook.isbn}</p>
+              <p>Added Date: {selectedBook.publishedDate}</p>
+              <button className={"btn_modal_close"} onClick={closeModal}>
+                Close
+              </button>
+            </div>
+          )}
         </div>
-    );
+      </Modal>
+    </div>
+  );
 };
 
 export default IssuedBooks;
--- Admin/src/pages/ReturnedBooks.js
+++ Admin/src/pages/ReturnedBooks.js
@@ -1,12 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const ReturnedBooks = () => {
-    return (
-        <div>
-            <h2>Page 3</h2>
-            <p>Content for Page 3</p>
-        </div>
-    );
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [books, setBooks] = useState(null);
+
+  const handleEdit = (book) => {
+    setSelectedBook(book);
+    setModalIsOpen(true);
+    console.log(`Edit book with ID ${book.id}`);
+  };
+
+  const handleDelete = (bookId) => {
+    console.log(`Delete book with ID ${bookId}`);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("/api/books");
+        if (!response.ok) {
+          throw Error("Failed to fetch books");
+        }
+        const json = await response.json();
+        setBooks(json);
+      } catch (error) {
+        console.error("Error fetching books", error.message);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  return (
+    <div className={"main_books container_flex_col"}>
+      <div className={"books_header"}>
+        <h2>Returned Books</h2>
+      </div>
+      <div className={"books_table_section"}>
+        <table>
+          <thead>
+            <tr>
+              <th className={"col_1"}>Book Name</th>
+              <th classN ame={"col_2"}>Member ID</th>
+              <th className={"col_3"}>Issued Date</th>
+              <th className={"col_4"}>Returned Date</th>
+              <th className={"col_5"}></th>
+              <th className={"col_6"}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {books &&
+              books.map((book) => (
+                <tr key={book.id}>
+                  <td>{book.bookName}</td>
+                  <td>{book.author}</td>
+                  <td>{book.isbn}</td>
+                  <td>{book.publishedDate}</td>
+                  <td>
+                    <br></br>
+                    <button
+                      className={"btn_books"}
+                      onClick={() => handleDelete(book.id)}
+                    >
+                      {/* <FontAwesomeIcon icon={faTrashAlt} /> */}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ReturnedBooks;
